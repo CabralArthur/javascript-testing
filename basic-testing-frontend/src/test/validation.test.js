56629ecf080565c6a9d1fd8712bd4f7cca495215
Arthur Cabral <63428806+CabralArthur@users.noreply.github.com>
2024-08-05 15:08:54 -0300

    ft: inserting validate number test cases

--- basic-testing-frontend/src/test/validation.test.js
+++ basic-testing-frontend/src/test/validation.test.js
@@ -24,3 +24,27 @@ if('Should throw an error when insert a string with blank spaces', () => {
 
     expect(result).toThrow(/Invalid input - must not be empty./);
 });
+
+it('Should run without throwing an error when insert a number', () => {
+    const value = 1;
+
+    const result = () => validateNumber(value);
+
+    expect(result).not.toThrow();
+});
+
+it('Should throw an error when insert a NaN value, (NaN, Object, String...)', () => {
+    const value = NaN;
+    const objectValue = {};
+    const stringValue = 'Invalid number';
+
+    const result = () => validateNumber(value);
+    const objectResult = () => validateNumber(objectValue);
+    const stringResult = () => validateNumber(stringValue);
+
+    const expectedErrorMessage = /Invalid number input./;
+
+    expect(result).toThrow(expectedErrorMessage);
+    expect(objectResult).toThrow(expectedErrorMessage);
+    expect(stringResult).toThrow(expectedErrorMessage);
+});
