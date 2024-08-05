fdd9265569cc424b187ae0cb23090e8ca24ed2ff
Arthur Cabral <63428806+CabralArthur@users.noreply.github.com>
2024-08-05 14:47:37 -0300

    ft: inserting multiple numbers test case

--- basic-testing-frontend/src/test/math.test.js
+++ basic-testing-frontend/src/test/math.test.js
@@ -47,3 +47,18 @@ it('Should return a correct sum if an array of numeric strings is provided', () 
     // 3rd A - Assert
     expect(result).toBe(expectedResult);
 });
+
+it('Should throw an error if a non-array is provided', () => {
+    const resultFunction = () => add();
+
+    expect(resultFunction).toThrow();
+});
+
+it('Should throw an error if provided multiple values instead an array', () => {
+    const num1 = 1;
+    const num2 = 2;
+
+    const resultFunction = () => add(num1, num2);
+
+    expect(resultFunction).toThrow();
+});
